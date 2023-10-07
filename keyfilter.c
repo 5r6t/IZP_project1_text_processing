e597e9e1d392361b917ec522e042b1b66bda2556
5r6t <mvtjr289@gmail.com>
2023-10-07 14:49:52 +0200

    learning stdin with Toaster, nothing else

--- keyfilter.c
+++ keyfilter.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char* argv[]) 
+int main() 
 {
-    for (int i = 0; i < argc; i++) 
-    {
-        printf("%s\n", argv[i]);
-    }      
+    
+
     return 0;
 }
