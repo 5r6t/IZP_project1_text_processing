b4cb96d57a242cedb3abca5abccd17d23e1898ea
5r6t <mvtjr289@gmail.com>
2023-10-10 19:33:40 +0200

    do it

--- keyfilter.c
+++ keyfilter.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/* https://www.youtube.com/watch?v=L-gRYBeA9YQ&feature=youtu.be&ab_channel=ToasterBro */
-
-int main() 
+int main(int argc, char* argv[]) 
 {
+    (void) argc;
+    (void) argv;
+    char buffer[101];
+    
+    while(fgets(buffer, 100, stdin) != NULL){
+        printf("%s", buffer);
+        if (!strcmp(buffer, "Praha\n")){
+            printf("Mame prahu, she aint leaving \n");
+        }
+    }    
     return 0;
-}
+
+}   
+
+    
