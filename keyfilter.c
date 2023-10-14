431a0add989cfade8807b8036b56c1ad5f1053f1
5r6t <mvtjr289@gmail.com>
2023-10-14 14:15:33 +0200

    didnt ask

--- keyfilter.c
+++ keyfilter.c
@@ -2,18 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char* argv[]) 
+int main(int argc, char** argv) 
 {
-    (void) argc;
     (void) argv;
-    char buffer[101];
+    // If too few arguments are given, end the program
+    if (argc < 2) {
+        fprintf(stderr, "Minimal ammount of arguments is two!");
+        return 1; 
+    }
+
     
-    while(fgets(buffer, 100, stdin) != NULL){
-        printf("%s", buffer);
-        if (!strcmp(buffer, "Praha\n")){
-            printf("Mame prah, she aint leaving \n");
-        }
-    }    
+    
+  
+    
+
     return 0;
 
 }   
