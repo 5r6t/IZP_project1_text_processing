b1e8a080a285bbf573e802693bb13a47bde9c78c
5r6t <mvtjr289@gmail.com>
2023-10-17 21:34:00 +0200

    sda

--- keyfilter.c
+++ keyfilter.c
@@ -4,31 +4,12 @@
 #include <ctype.h>
 #include <string.h>
 
+
+
 int main(int argc, char** argv) {
-
-    char buffer[101]; 
-    char result[100]; // Pre pripad prazdneho stringu, budu sa tu ukladat prve pismena
-    int outputAmount = 0;
-    (void) argv;
-    
     if (argc == 1) {
-        while (fgets(buffer, sizeof(buffer), stdin)) {
-            if (buffer[0] != '\0') {
-                result[outputAmount++] = buffer[0];
-            }
-        }
+        EmptyString();
     }
-    fprintf(stdout, "Enable:\n%s\n", result);
-
-    return 0;
-
-
-    /*
-    for (int i = 0; key[i]; i++) // user argument to UPPER CASE letters - case-insensitive
-    { 
-        key[i] = toupper(key[i]);
-    }
-    */
 }
 
     
